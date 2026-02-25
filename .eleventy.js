495649b352cccb72365410d19e0c2f10c8f4956d
Simeon Vincent <svincent@gmail.com>
2026-02-24 17:17:12 -0800

    Add copyrightYear shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/CNAME");
 
+  eleventyConfig.addShortcode("copyrightYear", () => {
+    return new Date().getFullYear();
+  });
+
   return {
     dir: {
       input: "src",
